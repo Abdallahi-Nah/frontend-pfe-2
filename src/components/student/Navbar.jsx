152a2dfb71334f4234783a5c11df0457d9549646
abdounah <abdounah102030@gmail.com>
2025-06-14 10:50:24 +0200

    Navbar cmponent

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h1>Navbar</h1>
+  return (
+    <div className="flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 bg-white">
+      <img
+        src={assets.fst}
+        alt="Logo"
+        className="w-8 lg:w-8 cursor-pointer"
+      />
+      <div className="hidden md:flex items-center gap-5 text-gray-500 ">
+        <div className="flex items-center gap-5">
+              <Link to="/my-enrollments">E-Etudiant</Link>
         </div>
-    )
-}
+          <button
+            className="bg-blue-600 text-white px-5 py-2 rounded-full cursor-pointer"
+          >
+            Créer un compte
+          </button>
+      </div>
 
-export default Navbar
+      <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
+        <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
+            <Link to="/my-enrollments">E-Etudiant</Link>
+        </div>
+          <button>
+            <img src={assets.user_icon} alt="" />
+          </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
