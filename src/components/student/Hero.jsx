0d40142cfba33826228ed2090406621980946ff9
sidimed <sidimedm76@gmail.com>
2025-06-21 11:15:18 +0000

    quelque modification

--- src/components/student/Hero.jsx
+++ src/components/student/Hero.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { assets } from "../../assets/assets";
-import SearchBar from "./SearchBar";
 
 const Hero = () => {
   return (
@@ -15,21 +14,28 @@ const Hero = () => {
       {/* Contenu centré superposé */}
       <div className="absolute inset-0 flex flex-col items-center justify-center px-4 md:px-8 text-center bg-black/40 space-y-5">
         <div className="flex flex-col items-center justify-center px-4 md:px-8 text-center bg-black/50 space-y-5 p-7 rounded-2xl">
-        <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold">
-          Faculté des Sciences et Techniques
-        </h1>
+          <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold">
+            Faculté des Sciences et Techniques
+          </h1>
 
-        <p className="text-gray-200 text-sm sm:text-base md:text-lg max-w-3xl">
-          Des enseignants qualifiés, des contenus interactifs et des formations
-          adaptées aux défis scientifiques actuels.
-        </p>
+          <p className="text-gray-200 text-sm sm:text-base md:text-lg max-w-3xl">
+            Des enseignants qualifiés, des contenus interactifs et des
+            formations adaptées aux défis scientifiques actuels.
+          </p>
 
-        <p className="text-gray-200 text-sm sm:text-base md:text-lg max-w-3xl">
-          Un environnement stimulant pour réussir vos objectifs académiques et
-          professionnels.
-        </p>
+          <p className="text-gray-200 text-sm sm:text-base md:text-lg max-w-3xl">
+            Un environnement stimulant pour réussir vos objectifs académiques et
+            professionnels.
+          </p>
 
-        <SearchBar />
+          <div className="flex items-center font-medium gap-6 mt-4">
+            <button className="px-10 py-3 rounded-md text-white bg-blue-600 cursor-pointer">
+              Commencer
+            </button>
+            <button className="flex items-center gap-2 cursor-pointer">
+              En savoir plus <img src={assets.arrow_icon} alt="arrow_icon" />
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,33 +1,36 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { AppContext } from "../../context/AppContext";
 
 const Navbar = () => {
+
+  const { navigate } = useContext(AppContext);
+
   return (
     <div className="flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 bg-white">
       <img
+        onClick={() => navigate("/")}
         src={assets.fst}
         alt="Logo"
         className="w-8 lg:w-8 cursor-pointer"
       />
       <div className="hidden md:flex items-center gap-5 text-gray-500 ">
         <div className="flex items-center gap-5">
-              <Link to="/my-enrollments">Mes Cours</Link>
+          <Link to="/my-enrollments">Mes Cours</Link>
         </div>
-          <button
-            className="bg-blue-600 text-white px-5 py-2 rounded-full cursor-pointer"
-          >
-            Créer un compte
-          </button>
+        <button className="bg-blue-600 text-white px-5 py-2 rounded-full cursor-pointer">
+          Créer un compte
+        </button>
       </div>
 
       <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
         <div className="flex items-center gap-1 sm:gap-2 max-sm:text-xs">
-            <Link to="/my-enrollments">E-Etudiant</Link>
+          <Link to="/my-enrollments">E-Etudiant</Link>
         </div>
-          <button>
-            <img src={assets.user_icon} alt="" />
-          </button>
+        <button>
+          <img src={assets.user_icon} alt="" />
+        </button>
       </div>
     </div>
   );
