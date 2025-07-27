cc2dd55063545451dd30c81e4e4153cf03d88166
Abdallahi Nah <abdallahi.nah.un.fst@gmail.com>
2025-07-27 19:04:32 +0000

    some changes

--- client/src/pages/educator/MyCourses.jsx
+++ client/src/pages/educator/MyCourses.jsx
@@ -653,8 +653,17 @@ const MyCourses = () => {
           <h2 className="text-2xl md:text-3xl font-bold text-gray-900">
             Mes cours
           </h2>
-          <button
+          {/* <button
             onClick={fetchAllCourses}
+            className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 text-sm"
+          >
+            Rafraîchir les cours
+          </button> */}
+          <button
+            onClick={async () => {
+              await fetchAllCourses();
+              setCourses([...allCourses]); // 🔁 forcer mise à jour locale
+            }}
             className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 text-sm"
           >
             Rafraîchir les cours
