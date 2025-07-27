0e383937e7966da1d15ed5d41ce030ee5890cfc1
Abdallahi Nah <abdallahi.nah.un.fst@gmail.com>
2025-07-27 11:22:40 +0000

    some changes

--- client/src/components/educator/Sidebar.jsx
+++ client/src/components/educator/Sidebar.jsx
@@ -166,7 +166,7 @@
 
 "use client";
 
-import { useContext, useState } from "react";
+import React, { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import {
   Menu,
